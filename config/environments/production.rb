27e204c9c46401e6c5442287c3e1afd6ce0fd9d7
Jerzy Mirecki <jerzy.mirecki@gmail.com>
2013-08-24 15:17:27 +0200

    Ignore the CSS compressor, change JS compressor to ":uglifier"

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,9 +11,8 @@ TibiaSmuggler::Application.configure do
   # Disable Rails's static asset server (Apache or nginx will already do this)
   config.serve_static_assets = false
 
-  # Compress JavaScripts and CSS
-  config.assets.css_compressor = :yui
-  config.assets.js_compressor = :uglify
+  # Compress JSs
+  config.assets.js_compressor = :uglifier
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
