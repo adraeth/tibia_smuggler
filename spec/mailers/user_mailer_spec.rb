8773cdb77855cdf5e5f5ef048a1770d91f110bd6
Jerzy Mirecki <jerzy.mirecki@gmail.com>
2013-08-27 19:56:45 +0200

    Improve tests for UserMailer

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -5,14 +5,12 @@ describe UserMailer do
   before { @user = User.new(name: 'Example User', email: 'user@example.com',
                             password: 'foobar', password_confirmation: 'foobar') }
 
-  let(:email) { UserMailer.signup_completed_email(@user) }
+  describe 'when a new account is created' do
 
-  describe 'when new account is created' do
     before { @user.save }
 
-    describe 'list of delivered emails' do
-      subject { ActionMailer::Base.deliveries }
-      it { should_not be_empty }
+    it 'an email notification should be sent' do
+      expect(ActionMailer::Base.deliveries.count).to eq(1)
     end
   end
 end
