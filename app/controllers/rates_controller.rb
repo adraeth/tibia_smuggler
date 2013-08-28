8050c945783db180a310e3381cd26d2524e208c0
Jerzy Mirecki <jerzy.mirecki@gmail.com>
2013-08-28 20:15:38 +0200

    Remove redundant dev variable from rates_controller.rb

--- app/controllers/rates_controller.rb
+++ app/controllers/rates_controller.rb
@@ -1,6 +1,5 @@
 class RatesController < ApplicationController
   def show
-    sleep 0 if Rails.env.development?
     @rate = rate_for(params)
     render json: @rate.to_json( only: [:rate, :reducible, :reduction_step])
   end
