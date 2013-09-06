293b373ebe8e34b8defd320e0962b70cf5a2dcda
Jerzy Mirecki <jerzy.mirecki@gmail.com>
2013-09-06 14:39:20 +0200

    Display correct Smuggler name in orders#show,index

--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -11,7 +11,7 @@ module OrdersHelper
   def get_smuggler_charname
     if @order.order_status_id.between?(2,4)
       link_to "Gold Smuggler #{@order.world_to.name}",
-            "http://www.tibia.com/community/?subtopic=characters&amp;name=Gold+Smuggler+#{@order.world_to.name}".html_safe
+            "http://www.tibia.com/community/?subtopic=characters&amp;name=Gold+Smuggler+#{@order.world_from.name}".html_safe
     else
       '---'
     end
